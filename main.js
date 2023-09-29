5897cf51609a86ba9d850586e32b0013d41e6895
Atonix0 <116169309+Atonix0@users.noreply.github.com>
2023-09-29 22:39:17 +0000

    Nah huh

--- main.js
+++ main.js
@@ -123,3 +123,14 @@ client.on(Events.InteractionCreate, async interaction => {
 
 
 client.login(token);
+//...Discord Bot Code Above ^^
+
+const express = require('express')
+const app = express();
+const port = 3000;
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
