2c370988dbef0c728769eb72bd580bd6a0dd10b7
Atonix0 <116169309+Atonix0@users.noreply.github.com>
2023-09-29 22:50:04 +0000

    hhk

--- deploy-commands.js
+++ deploy-commands.js
@@ -22,12 +22,9 @@ for (const folder of commandFolders) {
 	// Grab the SlashCommandBuilder#toJSON() output of each command's data for deployment
 	for (const file of commandFiles) {
 		const filePath = path.join(commandsPath, file);
-		const command = await import(filePath).default;
-		if ('data' in command && 'execute' in command) {
-			commands.push(command.data.toJSON());
-		} else {
-			console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
-		}
+		const command = await import(filePath);
+		
+		commands.push(command.default.data.toJSON());	
 	}
 }
 
